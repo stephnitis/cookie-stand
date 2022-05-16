1b7e82ade361410649c1853e38e895bd06cebfaa
Stephanie Hill <stephni.hill@gmail.com>
2022-05-16 15:25:39 -0700

    seattle sales calculator

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
 'use strict';
-
+let cookieSales = document.getElementById('location-sales');
+console.log(cookieSales);
 let hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
 
 // got from MDN docs
-function randomGuests(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function randomGuests(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
 let Seattle = {
   avgCookies: 6.3,
-  hrlyCookies: hrlyGuests * avgCookies,
-  hrlyGuests: randomGuests(23, 65),
- 
-}
-function cookiesPerhr() {
-for(let i = 0; i< this.hours.length; i++);
-total = '${hrlyCookies} at [i]';
-}
+  minCust: 23,
+  maxCust: 65,
+  hrlyGuests: function () {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  },
+  cookiesArray: [],
+  totalCookies: function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.cookiesArray.push(Math.ceil(this.hrlyGuests() * this.avgCookies));
+    }
+  },
+  dailyTotal: function () {
+    let sum = 0;
+    for (let i = 0; i < hours.length; i++) {
+      sum += this.cookiesArray[i],
+    }
+  },
+};
+
+Seattle.totalCookies();
 console.log(Seattle.avgCookies);
-console.log(Seattle.hrlyCookies);
-console.log(Seattle.hrlyGuests);
-console.log(total);
+console.log(Seattle.hrlyGuests());
+console.log(Seattle);
+console.log(Seattle.dailyTotal());
+
+// render: function(){
+//   let articleElem = document.createElement('article');
+// }
