02c97e1b2b841de3a6b9005411a46e935af1e83b
Stephanie Hill <stephni.hill@gmail.com>
2022-05-16 17:52:35 -0700

    tokyo cookie counter

--- js/app.js
+++ js/app.js
@@ -50,13 +50,56 @@ let cityOneSales = {
       ulElem.appendChild(liElem);
     }
 
-
   }
 };
 cityOneSales.totalCookies();
 cityOneSales.render();
 
-console.log(cityOneSales.hrlyGuests());
-console.log(cityOneSales);
-console.log(cityOneSales.dailyTotal());
+//console.log(cityOneSales.hrlyGuests());
+//console.log(cityOneSales);
+//console.log(cityOneSales.dailyTotal());
 
+let cityTwoSales = {
+  city: 'Tokyo',
+  avgCookies: 1.2,
+  minCust: 3,
+  maxCust: 24,
+  hrlyGuests: function () {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  },
+  cookiesArray: [],
+  totalCookies: function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.cookiesArray.push(Math.ceil(this.hrlyGuests() * this.avgCookies));
+    }
+  },
+  dailyTotal: function () {
+    let sum = 0;
+    for (let i = 0; i < this.cookiesArray.length; i++) {
+      console.log(sum);
+      sum += this.cookiesArray[i];
+    }
+    return sum;
+  },
+
+  render: function () {
+    let articleElem = document.createElement('article');
+    cookieSales.appendChild(articleElem);
+
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.city;
+    articleElem.appendChild(h2Elem);
+
+    let ulElem = document.createElement('ul');
+    articleElem.append(ulElem);
+
+    for (let i = 0; i < hours.length; i++) {
+      let liElem = document.createElement('li');
+      console.log(this.cookiesArray[i]);
+      liElem.textContent = `${hours[i]} ${this.cookiesArray[i]} Cookies`;
+      ulElem.appendChild(liElem);
+    }
+  }
+};
+cityTwoSales.totalCookies();
+cityTwoSales.render();
