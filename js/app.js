003a4237349b0ec2d6828d87bf667a912f5d1f19
Stephanie Hill <stephni.hill@gmail.com>
2022-05-19 12:28:32 -0700

    started table before realizing I was on the main branch

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let cookieSales = document.getElementById('location-sales');
 let tableElem = document.createElement('table');
 cookieSales.appendChild(tableElem);
+let cookieForm = document.getElementById('my-form');
 let hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
 
 // got from MDN docs
@@ -92,6 +93,20 @@ function renderAllCities() {
 }
 renderHeader();
 renderAllCities();
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let location = event.target.location.value;
+
+  let newCity = new CitySales(city,);
+}
+
+newCity.render();
+cookieForm.reset();
+
+cookieForm.addEventListener('submit', handleSubmit);
+
 
 //console.log(renderAllCities());
 
